17458eabf597fd2d398b9212be837f2c85bf20a1
Vasilii Stakrotckii <inferno681@gmail.com>
2024-03-24 12:16:35 +0100

    fix

--- pay2u/settings.py
+++ pay2u/settings.py
@@ -18,7 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'test_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '127.0.0.1').split(',')
 
