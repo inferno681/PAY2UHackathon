4e3f89b404d49ad0675e8ac57f82fa15414a28bf
Василий Стакроцкий <inferno681@yandex.ru>
2024-04-06 17:47:25 +0200

    tests

--- api/tests/test_api.py
+++ api/tests/test_api.py
@@ -25,31 +25,57 @@ SUBSCRIPTION_CREATE_NAME = 'subscription-list'
 SUBSCRIPTION_DETAIL_NAME = 'subscription-detail'
 SUBSCRIPTION_GET_REPORT_NAME = 'subscription-get-reciept'
 SUBSCRIPTION_CREATE_DATA = {
-    'id': 2,
+    'id': 3,
+    'period': MONTH,
+    'autorenewal': True
+}
+SUBSCRIPTION_UPDATE_DATA = {
     'period': MONTH,
     'autorenewal': True
 }
 COVER_LIST_RESULT = {
-    'count': 1,
+    'count': 3,
     'next': None,
     'previous': None,
     'results': [
         {
             'id': 1,
-            'name': 'cover_name',
+            'name': 'cover_name_1',
             'preview': 'preview',
             'logo_link': 'logo_link',
-            'price': 10.00,
-            'cashback_percent': 10.00,
+            'price': '10',
+            'cashback_percent': '10',
             'categories': [
                 1
+            ],
+            'is_subscribed': True
+        },
+        {
+            'id': 2,
+            'name': 'cover_name_2',
+            'preview': 'preview',
+            'logo_link': 'logo_link',
+            'price': '10',
+            'cashback_percent': '10',
+            'categories': [
+            ],
+            'is_subscribed': False
+        },
+        {
+            'id': 3,
+            'name': 'cover_name_3',
+            'preview': 'preview',
+            'logo_link': 'logo_link',
+            'price': '10',
+            'cashback_percent': '10',
+            'categories': [
             ],
             'is_subscribed': False
         }
     ]
 }
 COVER_DETAIL_RESULT = {
-    'name': 'cover_name',
+    'name': 'cover_name_1',
     'logo_link': 'logo_link',
     'service_link': 'service_link',
     'categories': [
@@ -58,9 +84,9 @@ COVER_DETAIL_RESULT = {
     'subscriptions': [
         {
             'id': 1,
-            'is_subscribed': False,
-            'end_date': None,
-            'name': 'subscription_name',
+            'is_subscribed': True,
+            'end_date': str(timezone.now().date() + timedelta(days=15)),
+            'name': 'active_subscription',
             'description': 'description',
             'monthly_price': '10.00',
             'semi_annual_price': '50.00',
@@ -75,7 +101,7 @@ MY_RESULT = {
     'first_name': '',
     'middle_name': '',
     'last_name': '',
-    'account_balance': '0.00',
+    'account_balance': '5000.00',
     'cashback': '0.00',
     'subscriptions': [
         {
@@ -111,19 +137,38 @@ MY_RESULT = {
     'current_month_expenses': None
 }
 SUBSCRIPTION_CREATE_RESULT = {
+    'id': 3,
+    'is_subscribed': True,
+    'end_date': str(timezone.now().date() + timedelta(days=30)),
+    'logo_link': 'logo_link',
+    'service_link': 'service_link',
+    'categories': [
+    ],
+    'period': MONTH,
+    'autorenewal': True,
+    'promocode': 'promocode1',
+    'cover_name': 'cover_name_3',
+    'name': 'new_subscription',
+    'description': 'description',
+    'monthly_price': '10.00',
+    'semi_annual_price': '50.00',
+    'annual_price': '95.00',
+    'cashback_percent': '10.00',
+    'cover': 3
+}
+SUBSCRIPTION_UPDATE_RESULT = {
     'id': 2,
     'is_subscribed': True,
     'end_date': str(timezone.now().date() + timedelta(days=30)),
     'logo_link': 'logo_link',
     'service_link': 'service_link',
     'categories': [
-        1
     ],
     'period': MONTH,
     'autorenewal': True,
     'promocode': 'promocode1',
     'cover_name': 'cover_name_2',
-    'name': 'subscription_second',
+    'name': 'inactive_subscription',
     'description': 'description',
     'monthly_price': '10.00',
     'semi_annual_price': '50.00',
@@ -145,7 +190,7 @@ SUBSCRIPTION_DETAIL_RESULT = {
     'autorenewal': True,
     'promocode': 'promocode1',
     'cover_name': 'cover_name_1',
-    'name': 'subscription_first',
+    'name': 'active_subscription',
     'description': 'description',
     'monthly_price': '10.00',
     'semi_annual_price': '50.00',
@@ -155,95 +200,16 @@ SUBSCRIPTION_DETAIL_RESULT = {
 }
 
 
-class GetTokenViewTests(APITestCase):
+class APITest(APITestCase):
     @classmethod
     def setUpTestData(cls):
 
         cls.user = User.objects.create(
-            username='user', phone_number='777', password='mypass')
-        cls.client = APIClient()
-
-    def test_post(self):
-        response = self.client.post(
-            '/api/auth/token/', data={'phone_number': self.user.phone_number})
-        self.assertEqual(response.status_code, 200)
-        token = str(RefreshToken.for_user(self.user).access_token)
-        self.assertEqual(len(token), len(response.data['token']))
-
-
-class CategoryViewSetTests(APITestCase):
-    @classmethod
-    def setUpTestData(cls):
-        cls.user = User.objects.create(
-            username='user', phone_number='777', password='mypass')
-        cls.client = APIClient()
-        cls.token = str(RefreshToken.for_user(cls.user).access_token)
-        cls.category = Category.objects.create(name='category')
-
-    def test_get_list(self):
-        response = self.client.get(
-            reverse(CATEGORY_LIST_NAME),
-            headers={'Authorization': f'Bearer {self.token}'}
+            username='user',
+            phone_number='777',
+            password='mypass',
+            account_balance=5000
         )
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, [{'id': 1, 'name': 'category'}])
-
-    def test_get_detail(self):
-        response = self.client.get(
-            reverse(CATEGORY_DETAIL_NAME, kwargs={'pk': self.category.pk}),
-            headers={'Authorization': f'Bearer {self.token}'}
-        )
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, {'id': 1, 'name': 'category'})
-
-
-class CoverViewSetTest(APITestCase):
-    @classmethod
-    def setUpTestData(cls):
-        cls.user = User.objects.create(
-            username='user', phone_number='777', password='mypass')
-        cls.client = APIClient()
-        cls.token = str(RefreshToken.for_user(cls.user).access_token)
-        cls.category = Category.objects.create(name='category')
-        cls.cover = Cover.objects.create(
-            name='cover_name',
-            preview='preview',
-            logo_link='logo_link',
-            service_link='service_link',
-        )
-        cls.cover.categories.add(cls.category)
-        cls.subscription = Subscription.objects.create(
-            name='subscription_name',
-            description='description',
-            monthly_price=10,
-            semi_annual_price=50,
-            annual_price=95,
-            cashback_percent=10,
-            cover=cls.cover
-        )
-
-    def test_get_list(self):
-        response = self.client.get(
-            reverse(COVER_LIST_NAME),
-            headers={'Authorization': f'Bearer {self.token}'}
-        )
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, COVER_LIST_RESULT)
-
-    def test_get_detail(self):
-        response = self.client.get(
-            reverse(COVER_DETAIL_NAME, kwargs={'pk': self.cover.pk}),
-            headers={'Authorization': f'Bearer {self.token}'}
-        )
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, COVER_DETAIL_RESULT)
-
-
-class UserViewTest(APITestCase):
-    @classmethod
-    def setUpTestData(cls):
-        cls.user = User.objects.create(
-            username='user', phone_number='777', password='mypass')
         cls.client = APIClient()
         cls.token = str(RefreshToken.for_user(cls.user).access_token)
         cls.category = Category.objects.create(name='category')
@@ -255,6 +221,12 @@ class UserViewTest(APITestCase):
         )
         cls.cover_2 = Cover.objects.create(
             name='cover_name_2',
+            preview='preview',
+            logo_link='logo_link',
+            service_link='service_link',
+        )
+        cls.cover_3 = Cover.objects.create(
+            name='cover_name_3',
             preview='preview',
             logo_link='logo_link',
             service_link='service_link',
@@ -278,6 +250,15 @@ class UserViewTest(APITestCase):
             cashback_percent=10,
             cover=cls.cover_2
         )
+        cls.new_subscription = Subscription.objects.create(
+            name='new_subscription',
+            description='description',
+            monthly_price=10,
+            semi_annual_price=50,
+            annual_price=95,
+            cashback_percent=10,
+            cover=cls.cover_3
+        )
         UserSubscription.objects.create(
             user=cls.user,
             subscription=cls.active_subscription,
@@ -299,7 +280,46 @@ class UserViewTest(APITestCase):
             promocode='promocode2'
         )
 
-    def test_get(self):
+    def test_receive_token(self):
+        response = self.client.post(
+            '/api/auth/token/', data={'phone_number': self.user.phone_number})
+        self.assertEqual(response.status_code, 200)
+        token = str(RefreshToken.for_user(self.user).access_token)
+        self.assertEqual(len(token), len(response.data['token']))
+
+    def test_get_categories_list(self):
+        response = self.client.get(
+            reverse(CATEGORY_LIST_NAME),
+            headers={'Authorization': f'Bearer {self.token}'}
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, [{'id': 1, 'name': 'category'}])
+
+    def test_get_category_detail(self):
+        response = self.client.get(
+            reverse(CATEGORY_DETAIL_NAME, kwargs={'pk': self.category.pk}),
+            headers={'Authorization': f'Bearer {self.token}'}
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, {'id': 1, 'name': 'category'})
+
+    def test_get_covers_list(self):
+        response = self.client.get(
+            reverse(COVER_LIST_NAME),
+            headers={'Authorization': f'Bearer {self.token}'}
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, COVER_LIST_RESULT)
+
+    def test_get_cover_detail(self):
+        response = self.client.get(
+            reverse(COVER_DETAIL_NAME, kwargs={'pk': self.cover_1.pk}),
+            headers={'Authorization': f'Bearer {self.token}'}
+        )
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, COVER_DETAIL_RESULT)
+
+    def test_get_user_info(self):
         response = self.client.get(
             reverse(USER_NAME),
             headers={'Authorization': f'Bearer {self.token}'}
@@ -307,63 +327,7 @@ class UserViewTest(APITestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.data, MY_RESULT)
 
-
-class SubscriptionViewSetTest(APITestCase):
-    @classmethod
-    def setUpTestData(cls):
-        cls.user = User.objects.create(
-            username='user',
-            phone_number='777',
-            password='mypass',
-            account_balance=5000
-        )
-        cls.client = APIClient()
-        cls.token = str(RefreshToken.for_user(cls.user).access_token)
-        cls.category = Category.objects.create(name='category')
-        cls.cover_1 = Cover.objects.create(
-            name='cover_name_1',
-            preview='preview',
-            logo_link='logo_link',
-            service_link='service_link',
-        )
-        cls.cover_2 = Cover.objects.create(
-            name='cover_name_2',
-            preview='preview',
-            logo_link='logo_link',
-            service_link='service_link',
-        )
-        cls.cover_1.categories.add(cls.category)
-        cls.cover_2.categories.add(cls.category)
-        cls.subscription_1 = Subscription.objects.create(
-            name='subscription_first',
-            description='description',
-            monthly_price=10,
-            semi_annual_price=50,
-            annual_price=95,
-            cashback_percent=10,
-            cover=cls.cover_1
-        )
-        cls.subscription_2 = Subscription.objects.create(
-            name='subscription_second',
-            description='description',
-            monthly_price=10,
-            semi_annual_price=50,
-            annual_price=95,
-            cashback_percent=10,
-            cover=cls.cover_2
-        )
-        UserSubscription.objects.create(
-            user=cls.user,
-            subscription=cls.subscription_1,
-            start_date=timezone.now().date() - timedelta(days=33),
-            end_date=timezone.now().date() + timedelta(days=15),
-            price=cls.subscription_1.monthly_price,
-            period=MONTH,
-            autorenewal=True,
-            promocode='promocode1'
-        )
-
-    def test_post(self):
+    def test_subscribe_new_subscription(self):
         with patch('api.tasks.send_sms_task.delay') as mock_apply_async:
             response = self.client.post(
                 reverse(SUBSCRIPTION_CREATE_NAME),
@@ -374,14 +338,30 @@ class SubscriptionViewSetTest(APITestCase):
             SUBSCRIPTION_CREATE_RESULT[
                 'promocode'
             ] = UserSubscription.objects.get(
-                subscription=self.subscription_2).promocode
+                subscription=self.new_subscription).promocode
             self.assertEqual(response.status_code, 201)
             self.assertEqual(response.data, SUBSCRIPTION_CREATE_RESULT)
 
-    def test_get_detail(self):
+    def test_subscribe_old_subscription(self):
+        with patch('api.tasks.send_sms_task.delay') as mock_apply_async:
+            response = self.client.patch(
+                reverse(SUBSCRIPTION_DETAIL_NAME,
+                        kwargs={'pk': self.inactive_subscription.pk}),
+                data=SUBSCRIPTION_UPDATE_DATA,
+                headers={'Authorization': f'Bearer {self.token}'}
+            )
+            mock_apply_async.assert_called_once
+            SUBSCRIPTION_UPDATE_RESULT[
+                'promocode'
+            ] = UserSubscription.objects.get(
+                subscription=self.inactive_subscription).promocode
+            self.assertEqual(response.status_code, 200)
+            self.assertEqual(response.data, SUBSCRIPTION_UPDATE_RESULT)
+
+    def test_get_subscription_detail(self):
         response = self.client.get(
             reverse(SUBSCRIPTION_DETAIL_NAME, kwargs={
-                    'pk': self.subscription_1.pk}),
+                    'pk': self.active_subscription.pk}),
             headers={'Authorization': f'Bearer {self.token}'}
         )
         self.assertEqual(response.status_code, 200)
@@ -390,13 +370,13 @@ class SubscriptionViewSetTest(APITestCase):
     def test_get_reciept(self):
         response = self.client.get(
             reverse(SUBSCRIPTION_GET_REPORT_NAME, kwargs={
-                    'pk': self.subscription_1.pk}),
+                    'pk': self.active_subscription.pk}),
             headers={'Authorization': f'Bearer {self.token}'}
         )
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response['content-type'], 'application/pdf')
         self.assertIn('attachment', response['content-disposition'])
         self.assertIn(
-            f'Reciept#{self.subscription_1.id}.pdf',
+            f'Reciept#{self.active_subscription.id}.pdf',
             response['content-disposition']
         )
